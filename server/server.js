0fccb43aa2355eb7eb327870ead0a56606981d6b
shimonaeman <shimonaeman@gmail.com>
2017-07-06 11:09:08 +0300

    modiefid to work online

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express=require('express');
+var bp=require('body-parser');
+
+var {mongose}=require('./db/mongoose');
+var {Todo}=require('./models/Todo');
+var {user} =require('./models/User');
+
+var app=express();
+
+app.use(bp.json());
+
+app.post('/todos',(req,res)=>{
+ var todo= new Todo({
+   text: req.body.text
+ });
+ todo.save().then((doc)=>{
+   res.send(doc);
+ },(e)=>{
+   res.status(400).send(e);
+ });
+});
+
+
+app.listen(3000,()=>{
+console.log("strat on port 3000");
+
+});
