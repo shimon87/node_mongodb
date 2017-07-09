ae29683c12961c8a5cc52c961ae025b7b2595c52
shimonaeman <shimonaeman@gmail.com>
2017-07-09 10:46:12 +0300

    add get all todo func

--- server/server.js
+++ server/server.js
@@ -13,12 +13,24 @@ app.post('/todos',(req,res)=>{
  var todo= new Todo({
    text: req.body.text
  });
+
  todo.save().then((doc)=>{
    res.send(doc);
  },(e)=>{
    res.status(400).send(e);
  });
 });
+
+
+ app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+      res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  })
+  });
+
+
 
 
 app.listen(3000,()=>{
