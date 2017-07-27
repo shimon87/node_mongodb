58166965807bdb4b2f70d775ec6f26ab8e833968
shimonaeman <shimonaeman@gmail.com>
2017-07-27 14:44:47 +0300

    add new api

---
+++ server/models/albums.js
@@ -0,0 +1,36 @@
+var mongoose =require('mongoose');
+
+var User = mongoose.model('album',{
+    name:{
+      type:String,
+      required: true,
+      trim: true,
+      minlength:1
+    }
+    url:{
+        type:String,
+        required: true,
+        trim: true,
+        minlength: 1
+
+    }
+    img:{
+        type:String,
+        required: true,
+        trim: true,
+        minlength: 1
+
+    }
+    tiny_img:{
+        type:String,
+        required: true,
+        trim: true,
+        minlength: 1
+
+    }
+});
+
+
+module.exports ={
+album
+};
