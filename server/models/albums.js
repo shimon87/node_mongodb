17e4d11358a1f92368b1d68c88725e9c05803a2d
shimonaeman <shimonaeman@gmail.com>
2017-07-30 11:50:09 +0300

    fix bugs

--- server/models/albums.js
+++ server/models/albums.js
@@ -6,21 +6,21 @@ var album = mongoose.model('album',{
       required: true,
       trim: true,
       minlength:1
-    }
+    },
     url:{
         type:String,
         required: true,
         trim: true,
         minlength: 1
 
-    }
+    },
     img:{
         type:String,
         required: true,
         trim: true,
         minlength: 1
 
-    }
+    },
     tiny_img:{
         type:String,
         required: true,
